e9b85c9c88963f97d3ee05ca0522f7b5572284fd
TejaswiV <tejaswi.venupalli@gmail.com>
2019-01-31 15:55:03 +1100

    uploading python script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+from io import BytesIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.tejaswivenupalli.info')
+build_bucket = s3.Bucket('portfoliobuild.tejaswi.info')
+
+portfolio_zip = BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        portfolio_bucket.upload_fileobj(obj,name,
+        ExtraArgs={'ContentType':mimetypes.guess_type(name)[0]})
+        portfolio_bucket.Object(name).Acl().put(ACL="public-read")
